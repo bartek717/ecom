148b608dd6b4a261e30f7dfbadf777d9cea7f6b2
bartekkowalski465 <bartekkowalski925@gmail.com>
2023-03-06 23:10:58 -0500

    done!

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,8 +2,17 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+console.log(process.env.REACT_APP_apiKey)
+console.log(process.env)
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
